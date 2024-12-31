54063055ac0cb1993c90c2a9eedfa08facac03c3
Сергей Стародубцев <s@runtel.ru>
2024-12-31 04:48:09 +0300

    test

--- test/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Tests package initialization.
-Contains test configurations and utilities.
-"""
-import os
-import sys
-
-# Добавляем путь к исходному коду в PYTHONPATH
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
